f49090420da47897fada48cd1eb1968be458c716
vpanchuk <v.panchuk@pozitiff.ua>
2020-08-21 16:37:00 +0300

    add asset box view

--- src/components/poolNew/assetNew.jsx
+++ src/components/poolNew/assetNew.jsx
@@ -29,11 +29,39 @@ const store = Store.store
 
 const styles = theme => ({
   value: {
-    cursor: 'pointer'
+    cursor: 'pointer',
+    textAlign: 'left',
+    fontWeight: 'normal',
+    fontSize: '14px',
+    lineHeight: '22px',
+    color: '#252626',
+    marginTop: '5px',
   },
   actionInput: {
-    padding: '0px 0px 12px 0px',
-    fontSize: '0.5rem'
+    fontSize: '14px',
+    lineHeight: '22px',
+    color: '#888A8C',
+    '& input': {
+      padding: '11px',
+      fontWeight: 'normal',
+      borderRadius: '6px',
+    },
+    '& div:hover:before': {
+      borderBottom: 'none',
+      boxShadow: 'none',
+    },
+    '& div:before': {
+      border: 'none',
+      boxShadow: 'none',
+    },
+    '& div:after': {
+      border: 'none',
+      boxShadow: 'none',
+    },
+    '& div': {
+      borderRadius: '6px',
+      background: '#F3F4F5',
+    }
   },
   balances: {
     width: '100%',
@@ -42,9 +70,10 @@ const styles = theme => ({
     cursor: 'pointer'
   },
   actionsContainer: {
-    paddingBottom: '12px',
     display: 'flex',
+    justifyContent: 'space-between',
     flex: '1',
+    marginTop: '23px',
     [theme.breakpoints.down('sm')]: {
       flexDirection: 'column'
     }
@@ -53,13 +82,29 @@ const styles = theme => ({
     paddingRight: '24px'
   },
   actionButton: {
-    height: '47px'
+    borderRadius: '4px',
+    margin: '10px 5px 0',
+    padding: '8px 13px',
+    maxWidth: 'max-content',
+    alignItems: 'center',
+    whiteSpace: 'nowrap',
+    '&:first-of-type': {
+      marginLeft: '0',
+    },
+    '&:last-of-type': {
+      marginRight: '0',
+    },
+    '&.MuiButton-contained.Mui-disabled':{
+      color: '#b1b1b1',
+    },
   },
   tradeContainer: {
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center'
+    alignItems: 'center',
+    maxWidth: '275px',
+    width: '100%',
   },
   sepperator: {
     borderBottom: '1px solid #E1E1E1',
@@ -71,7 +116,11 @@ const styles = theme => ({
     }
   },
   percentSlider: {
-    width: 300
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center', 
+    justifyContent: 'space-between',
+    marginTop: '30px', 
   },
   scaleContainer: {
     display: 'flex',
@@ -84,7 +133,10 @@ const styles = theme => ({
     minWidth: '10px'
   },
   buttonText: {
-    fontWeight: '700',
+    fontWeight: '600',
+    fontSize: '16px',
+    lineHeight: '24px',
+    marginLeft: '5px',
   },
   headingContainer: {
     width: '100%',
@@ -106,12 +158,75 @@ const styles = theme => ({
   },
   buttons: {
     display: 'flex',
-    width: '100%'
+    width: '100%',
+    marginTop: '20px',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
   },
   disabledContainer: {
     width: '100%',
     paddingTop: '12px',
     textAlign: 'center'
+  },
+  box: {
+    background: '#fff',
+    boxShadow: '0px 4px 26px rgba(83, 29, 171, 0.29)',
+    borderRadius: '4px',
+    padding: '25px 40px',
+    maxWidth: '695px',
+    width: '100%',
+    margin: '0 auto'
+  },
+  assetHeader: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    borderBottom: '1px solid #D5D7D9',
+    paddingBottom: '20px',
+  },
+  assetTitle: {
+    maxWidth: '340px',
+    width: '100%',
+    fontWeight: 'normal',
+    fontSize: '16px',
+    lineHeight: '24px',
+    color: '#252626',
+    marginLeft: '15px',
+  },
+  description: {
+    fontWeight: 'normal',
+    fontSize: '12px',
+    lineHeight: '20px',
+    textAlign: 'center',
+    color: '#252626',
+    marginTop: '19px',
+  },
+  percentValue: {
+    fontWeight: 'normal',
+    fontSize: '14px',
+    lineHeight: '22px',
+    alignItems: 'center',
+    color: '#595959',
+    marginLeft: '8px'
+  },
+  slider:{
+    width: '83%',
+    '& .MuiSlider-rail':{
+      height: '8px',
+      background: '#dcdcdc',
+    },
+    '& .MuiSlider-track':{
+      height: '8px',
+      background: '#1890FF',
+    },
+    '& .MuiSlider-mark':{
+      height: '14px',
+      width: '1px',
+      background: '#D5D7D9',
+      marginTop: '-3px'
+    },
+    '& .MuiSlider-thumb':{
+      display: 'none',
+    }
   }
 });
 
@@ -174,16 +289,15 @@ class Asset extends Component {
     } = this.state
 
     return (
-      <>
-        <div>
+      <div className={ classes.box }> 
+        <div className={ classes.assetHeader }>
           <img
             alt=""
             src={require('../../assets/' + asset.symbol + '-logo.png')}
             height={'40px'}
             style={asset.disabled ? { filter: 'grayscale(100%)' } : {}}
           />
-          <p>Choose an asset in the table below and</p>
-          <p>deposit it so YFI can put it to work for you</p>
+          <Typography className={ classes.assetTitle } variant={ 'h6'}>Choose an asset in the table below and deposit it so YFI can put it to work for you</Typography>
         </div>
         <div className={ classes.actionsContainer }>
           <div className={ classes.tradeContainer }>
@@ -191,12 +305,12 @@ class Asset extends Component {
               fullWidth
               className={ classes.actionInput }
               id='amount'
+              variant="filled"
               value={ amount }
               error={ amountError }
               onChange={ this.onChange }
               disabled={ loading || asset.disabled }
               placeholder="0.00"
-              variant="outlined"
               onKeyDown={ this.inputKeyDown }
             />
             {!asset.disabled && <div className={ classes.balances }>
@@ -205,6 +319,7 @@ class Asset extends Component {
             <div className={ classes.percentSlider }>
               <Slider
                 defaultValue={0}
+                className={ classes.slider }
                 aria-labelledby="discrete-slider"
                 step={25}
                 marks
@@ -216,31 +331,31 @@ class Asset extends Component {
                   this.setAmount(value)
                 }}
               />
-              <span>{leftSlider}%</span>
+              <Typography className={ classes.percentValue } variant={ 'h6'}>{leftSlider}%</Typography>
             </div>
             <div className={ classes.buttons }>
               <Button
                 className={ classes.actionButton }
-                variant="outlined"
+                variant="contained"
                 color="primary"
                 disabled={ loading || !account.address || asset.balance <= 0 || asset.depositDisabled === true }
                 onClick={ this.onDeposit }
                 fullWidth
               >
                 <img src={require(`../../assets/ico-deposit.svg`)} alt="" />
-                <Typography className={ classes.buttonText } variant={ 'h5'} color={asset.disabled?'':'secondary'}>Deposit</Typography>
+                <Typography className={ classes.buttonText } variant={ 'h5'} color={asset.disabled?'':''}>Deposit</Typography>
               </Button>
               { asset.version === 2 &&
                 <Button
                   className={ classes.actionButton }
-                  variant="outlined"
+                  variant="contained"
                   color="primary"
                   disabled={ loading || !account.address || asset.balance <= 0 || asset.depositDisabled === true }
                   onClick={ this.onDepositAll }
                   fullWidth
                   >
                   <img src={require(`../../assets/ico-deposit.svg`)} alt="" />
-                  <Typography className={ classes.buttonText } variant={ 'h5'} color={asset.disabled?'':'secondary'}>Deposit All</Typography>
+                  <Typography className={ classes.buttonText } variant={ 'h5'} color={asset.disabled?'':''}>Deposit All</Typography>
                 </Button>
               }
             </div>
@@ -249,7 +364,7 @@ class Asset extends Component {
                 <Typography variant='h4'>Deposits are currently disabled for this vault</Typography>
               </div>
             }
-            <p>Upon deposit, assets are wrapped as yTokens in your wallet representing liquidity provided</p>
+            <Typography className={ classes.description } variant='h6'>Upon deposit, assets are wrapped as yTokens in your wallet representing liquidity provided</Typography>
           </div>
           <div className={ classes.sepperator }></div>
           <div className={classes.tradeContainer}>
@@ -262,14 +377,15 @@ class Asset extends Component {
               onChange={ this.onChange }
               disabled={ loading }
               placeholder="0.00"
-              variant="outlined"
+              variant="filled"
               onKeyDown={ this.inputRedeemKeyDown }
             />
             <div className={ classes.balances }>
-              <Typography variant='h4' className={ classes.value } noWrap>{ asset.pooledBalance ? (Math.floor(asset.pooledBalance*10000)/10000).toFixed(4) : '0.0000' } { asset.poolSymbol } </Typography>
+              <Typography variant='h4' className={ classes.value } noWrap>{ 'Balance: ' + (asset.pooledBalance ? (Math.floor(asset.pooledBalance*10000)/10000).toFixed(4) : '0.0000') } { asset.poolSymbol } </Typography>
             </div>
             <div className={ classes.percentSlider }>
               <Slider
+                className={ classes.slider }
                 defaultValue={0}
                 aria-labelledby="discrete-slider"
                 step={25}
@@ -282,38 +398,38 @@ class Asset extends Component {
                   this.setRedeemAmount(value)
                 }}
               />
-              <span>{rightSlider}%</span>
+              <Typography className={ classes.percentValue } variant={ 'h6'}>{rightSlider}%</Typography>
             </div>
             <div className={ classes.buttons }>
               <Button
                 className={ classes.actionButton }
-                variant="outlined"
+                variant="contained"
                 color="primary"
                 disabled={ loading || !account.address || asset.pooledBalance <= 0 }
                 onClick={ this.onWithdraw }
                 fullWidth
                 >
                 <img src={require(`../../assets/ico-withdraw.svg`)} alt="" />
-                <Typography className={ classes.buttonText } variant={ 'h5'} color='secondary'>Withdraw</Typography>
+                <Typography className={ classes.buttonText } variant={ 'h5'} color=''>Withdraw</Typography>
               </Button>
               { asset.version === 2 &&
                 <Button
                   className={ classes.actionButton }
-                  variant="outlined"
+                  variant="contained"
                   color="primary"
                   disabled={ loading || !account.address || asset.pooledBalance <= 0 }
                   onClick={ this.onWithdrawAll }
                   fullWidth
                   >
                   <img src={require(`../../assets/ico-withdraw.svg`)} alt="" />
-                  <Typography className={ classes.buttonText } variant={ 'h5'} color='secondary'>Withdraw All</Typography>
+                  <Typography className={ classes.buttonText } variant={ 'h5'} color=''>Withdraw All</Typography>
                 </Button>
               }
             </div>
-            <p>There is a 0.5% withdrawal fee on all vaults, and a 5% performance fee on subsidized gas.</p>
+            <Typography className={ classes.description } variant='h6'>There is a 0.5% withdrawal fee on all vaults, and a 5% performance fee on subsidized gas.</Typography>
           </div>
         </div>
-      </>
+      </div>
     )
   };
 
