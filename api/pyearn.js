ac858ebc66c937e499172fb05e71812f866bb3f2
Petro Kuzmychev <johnnyfearmekramer@gmail.com>
2020-08-22 22:40:17 +0400

    WIP update pyearn func

--- api/pyearn.js
+++ api/pyearn.js
@@ -1,7 +1,21 @@
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
+const axios = require('axios')
+
+module.exports = async (req, res) => {
+  const data = (await axios({
+    method: 'GET',
+    url: 'https://py-earn.herokuapp.com/',
+  })).data
+  const symbols = []
+  for (let i = 0; i < data.props.children.length; i += 1) {
+      const arr1 = data.props.children
+      if (arr1[i].type === 'Center') symbols.push(arr1[i].props.children[0].props.children)
+  }
+  const values = []
+  for (let i = 0; i < data.props.children.length; i += 1) {
+      const arr1 = data.props.children
+      if (arr1[i].type === 'Table') values.push(arr1[i].props.children[1].props.children[2].props.children[3].props.children)
+  }
+  return res.json({
+    body: symbols.map((s, i) => ({ symbol: s, value: values[i] })),
   })
 }
