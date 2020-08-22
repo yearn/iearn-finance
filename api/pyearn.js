2312e0c5b690419b4aeb84edce328f14390ac41b
Petro Kuzmychev <johnnyfearmekramer@gmail.com>
2020-08-22 23:13:54 +0400

    WIP update pyearn func

--- api/pyearn.js
+++ api/pyearn.js
@@ -1,21 +1,26 @@
 const axios = require('axios')
 
 module.exports = async (req, res) => {
-  const data = (await axios({
-    method: 'GET',
-    url: 'https://py-earn.herokuapp.com/',
-  })).data
   const symbols = []
-  for (let i = 0; i < data.props.children.length; i += 1) {
-      const arr1 = data.props.children
-      if (arr1[i].type === 'Center') symbols.push(arr1[i].props.children[0].props.children)
-  }
   const values = []
-  for (let i = 0; i < data.props.children.length; i += 1) {
-      const arr1 = data.props.children
-      if (arr1[i].type === 'Table') values.push(arr1[i].props.children[1].props.children[2].props.children[3].props.children)
+  try {
+    const data = (await axios({
+      method: 'get',
+      url: 'https://py-earn.herokuapp.com/',
+    })).data
+  
+    for (let i = 0; i < data.props.children.length; i += 1) {
+        const arr = data.props.children
+        if (arr[i].type === 'Center') symbols.push(arr[i].props.children[0].props.children)
+        if (arr[i].type === 'Table') values.push(arr[i].props.children[1].props.children[2].props.children[3].props.children)
+    }
+    return res.json({
+      body: { success: true, data: symbols.map((s, i) => ({ symbol: s, value: values[i] })) },
+    })
+  } catch (e) {
+    console.error('ERROR', e.toString())
+    return res.json({
+      body: { success: false, error: e.toString() },
+    })
   }
-  return res.json({
-    body: symbols.map((s, i) => ({ symbol: s, value: values[i] })),
-  })
 }
