367f44647086758566686cc3c02934433840d47c
Anton Nell <antonnell.crypto@gmail.com>
2020-09-01 12:59:18 +0200

    Added footer links
    Signed-off-by: Anton Nell <antonnell.crypto@gmail.com>

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -20,42 +20,27 @@ const styles = theme => ({
   footer: {
     padding: '24px',
     display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-end',
+    justifyContent: 'space-evenly',
     width: '100%',
-    alignItems: 'center',
-    [theme.breakpoints.up('sm')]: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      width: '100%',
-      alignItems: 'center',
-    }
+    background: colors.white,
+    borderRadius: '50px 50px 0px 0px',
+    border: '1px solid '+colors.borderBlue,
+    borderBottom: 'none',
+    marginTop: '12px'
   },
-  footerLinks: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    maxWidth: '420px'
-  },
-  footerText: {
-    cursor: 'pointer',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '14px',
-    }
-  },
-  languageContainer: {
-    paddingLeft: '12px',
-    display: 'none'
-  },
-  selectInput: {
-    fontSize: '14px',
-    color: colors.pink
+  heading: {
+    marginBottom: '12px',
+    paddingBottom: '9px',
+    borderBottom: "3px solid "+colors.borderBlue,
   },
   link: {
-    textDecoration: 'none'
+    paddingBottom: '12px',
+    cursor: 'pointer',
+    '&:hover': {
+      textDecoration: 'underline'
+    }
   }
+
 });
 
 
@@ -65,8 +50,6 @@ class Footer extends Component {
     super()
 
     this.state = {
-      languages: store.getStore('languages'),
-      language: 'en',
       modalBuiltWithOpen: false,
     }
   }
@@ -74,9 +57,7 @@ class Footer extends Component {
   render() {
     const { classes, t, location } = this.props;
     const {
-      modalBuiltWithOpen,
-      languages,
-      language
+      modalBuiltWithOpen
     } = this.state
 
     if(location.pathname === '' || location.pathname === '/') {
@@ -85,28 +66,30 @@ class Footer extends Component {
 
     return (
       <div className={classes.footer}>
-        <div className={classes.footerLinks}>
-          <Typography onClick={()=> window.open("https://docs.yearn.finance", "_blank")} className={ classes.footerText } variant={ 'h6'}>{ t('Footer.About') }</Typography>
-          <Typography onClick={()=> window.open("https://docs.yearn.finance", "_blank")} className={ classes.footerText } variant={ 'h6'}>{ t('Footer.Docs') }</Typography>
-          <Typography onClick={()=> window.open("https://github.com/iearn-finance", "_blank")} className={ classes.footerText } variant={ 'h6'}>{ t('Footer.Code') }</Typography>
-          <Typography onClick={()=> window.open("https://t.me/iearnfinance", "_blank")} className={ classes.footerText } variant={ 'h6'}>{ t('Footer.Telegram') }</Typography>
-          <Typography onClick={ this.builtWithOverlayClicked } className={ classes.footerText } variant={ 'h6'}>{ t('Footer.BuiltWith') }</Typography>
+        <div className={ classes.socials }>
+          <Typography className={ classes.heading } variant={ 'h6'}>Our products</Typography>
+          <Typography onClick={()=> window.open("https://yearn.finance", "_blank")} className={ classes.link } variant={ 'h4'} >yearn.finance</Typography>
+          <Typography onClick={()=> window.open("https://ygov.finance", "_blank")} className={ classes.link } variant={ 'h4'} >ygov.finance</Typography>
+          <Typography onClick={()=> window.open("https://yinsure.finance", "_blank")} className={ classes.link } variant={ 'h4'} >yinsure.finance</Typography>
+          <Typography onClick={()=> window.open("https://yborrow.finance", "_blank")} className={ classes.link } variant={ 'h4'} >yborrow.finance</Typography>
+          <Typography onClick={()=> window.open("https://docs.yearn.finance", "_blank")} className={ classes.link } variant={ 'h4'} >docs.yearn.finance</Typography>
         </div>
-        <div className={ classes.languageContainer }>
-          <FormControl variant="outlined">
-            <Select
-              id="language"
-              value={ language }
-              onChange={ this.handleLanguageChange }
-              inputProps={{ className: classes.selectInput }}
-              color="primary"
-              fullWidth
-            >
-              { languages.map((language) => {
-                return <MenuItem key={ language.code } value={ language.code }>{ language.language }</MenuItem>
-              })}
-            </Select>
-          </FormControl>
+        <div className={ classes.socials }>
+          <Typography className={ classes.heading } variant={ 'h6'}>Community products</Typography>
+          <Typography onClick={()=> window.open("https://ycosystem.info", "_blank")} className={ classes.link } variant={ 'h4'} >ycosystem.info</Typography>
+          <Typography onClick={()=> window.open("https://www.learnyearn.finance", "_blank")} className={ classes.link } variant={ 'h4'} >learnyearn.finance</Typography>
+          <Typography onClick={()=> window.open("https://stats.finance/yearn", "_blank")} className={ classes.link } variant={ 'h4'} >stats.finance</Typography>
+          <Typography onClick={()=> window.open("https://yieldfarming.info", "_blank")} className={ classes.link } variant={ 'h4'} >yieldfarming.info</Typography>
+          <Typography onClick={()=> window.open("https://feel-the-yearn.app", "_blank")} className={ classes.link } variant={ 'h4'} >feel-the-yearn.app</Typography>
+          <Typography onClick={()=> window.open("https://yearn.snapshot.page", "_blank")} className={ classes.link } variant={ 'h4'} >yearn.snapshot.page</Typography>
+        </div>
+        <div className={ classes.socials }>
+          <Typography className={ classes.heading } variant={ 'h6'}>Socials</Typography>
+          <Typography onClick={()=> window.open("https://twitter.com/iearnfinance", "_blank")} className={ classes.link } variant={ 'h4'} >Twitter</Typography>
+          <Typography onClick={()=> window.open("https://medium.com/iearn", "_blank")} className={ classes.link } variant={ 'h4'} >Medium</Typography>
+          <Typography onClick={()=> window.open("https://discord.gg/GcjxhWR", "_blank")} className={ classes.link } variant={ 'h4'} >Discord</Typography>
+          <Typography onClick={()=> window.open("https://t.me/yearnfinance", "_blank")} className={ classes.link } variant={ 'h4'} >Telegram</Typography>
+          <Typography onClick={()=> window.open("https://github.com/iearn-finance", "_blank")} className={ classes.link } variant={ 'h4'} >Github</Typography>
         </div>
         { modalBuiltWithOpen && this.renderBuiltWithModal() }
       </div>
@@ -125,14 +108,6 @@ class Footer extends Component {
 
   closeBuiltWithModal = () => {
     this.setState({ modalBuiltWithOpen: false })
-  }
-
-  handleLanguageChange = (event) => {
-    let val = []
-    val.language = event.target.value
-    this.setState(val)
-
-    i18n.changeLanguage(event.target.value)
   }
 }
 
